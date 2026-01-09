245907bb337c292806b0a3b2cc8a30928a10094d
Andy Soelistio <andy.soelistio@gmail.com>
2026-01-09 10:48:54 +0700

    feat: Yearly Revenues of store 1 and 2

--- config_local_docker.py
+++ config_local_docker.py
@@ -2,5 +2,5 @@
 DB_USER = "postgres"  
 DB_PASSWORD = "postgres"
 DB_HOST = "localhost"       # local use localhost; pure docker use postgres # IMPORTANT: Docker internal hostname
-DB_PORT = 5450              # run from local machine  
+DB_PORT =  5450              # run from local machine  
 DB_NAME = "ai_rental_db"
